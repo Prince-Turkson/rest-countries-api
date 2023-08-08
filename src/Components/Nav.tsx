8dd8f478ebaa9564dc1c8d155a7d924155a8d190
PrinceCharlesTurkson <Elizabethben793@gmail.com>
2023-08-08 09:42:48 +0000

    completed the navbar

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../Styles/navBar.css'
+import Toggle from './Toggle';
 
 const Nav = () => {
   return (
-    <div>
-      
+    <div className='navBar'>
+        <nav>
+            <h1>Where in the world?</h1>
+        </nav>
+        <Toggle />
     </div>
   );
 }
--- src/Components/Toggle.tsx
+++ src/Components/Toggle.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {FaSun, FaMoon} from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import '../Styles/button.css';
+
 
 const Toggle = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    const [darkMode, setDarkMode] = useState(false)
+    const ToogleChange = () =>{
+        setDarkMode(prevDarkMode => !prevDarkMode)
+    }
+    
+    useEffect(() => {
+        
+        document.documentElement.classList.toggle('dark-mode', darkMode);
+      }, [darkMode]);
+    
+    return (
+        <div>
+        <button className='toogle-button' onClick={ToogleChange}>
+           {darkMode ? <FaSun /> : <FaMoon /> } 
+           {darkMode ? <span>Light Mode</span> : <span>Dark Mode</span>}
+         
+        </button>
+
+
+        </div>
+    )
 }
 
 export default Toggle;
